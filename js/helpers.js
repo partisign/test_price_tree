5e6533a83ba7e36d31fa7184ab77d9852ad397d6
Misha Morozov <ds@chelny.com>
2024-02-24 16:53:23 +0300

    sort helper

---
+++ js/helpers.js
@@ -0,0 +1,8 @@
+﻿export function sortAray(array) {
+  array.sort((a, b) => a.sorthead - b.sorthead);
+  array.sort((a, b) => {
+    if (a.node === b.node || (a.node > 0 && b.node > 0)) {
+      return 0;
+    } else return b.node - a.node;
+  });
+}
--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 ﻿import { fetchData } from "./fetchDataService.js";
+import { sortAray } from "./helpers.js";
 const { services } = await fetchData();
+sortAray(services);
 console.table(services);
 const rootElement = document.getElementById("tree");
